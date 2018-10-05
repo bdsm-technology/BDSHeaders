d5af0a5eb7868fdbb61ca86dad0876b218588042
CodeHz <codehz@outlook.com>
2018-10-05 14:59:51 +0800

    base commit
    Signed-off-by: CodeHz <codehz@outlook.com>

---
+++ hook.h
@@ -0,0 +1,39 @@
+#pragma once
+
+template <typename T> static void *void_cast(T f) {
+  union {
+    T pf;
+    void *p;
+  };
+  pf = f;
+  return p;
+}
+
+void *MinecraftHandle() {
+  static void *handle = dlopen(nullptr, RTLD_LAZY);
+  return handle;
+}
+
+extern "C" {
+
+void *MSHookFunction(void *symbol, void *hook, void **original);
+
+#define CONS(a, b) a##b
+#define HOOK(type, method) mcpelauncher_hook(void_cast(&type ::method), void_cast(&type ::CONS($$, method)), (void **)&type ::CONS($, method)())
+#define IMPL(type, ret, name, suffix, ...)                                                                                                           \
+  using CONS(name, _t) = ret (*)(type suffix *, ##__VA_ARGS__);                                                                                      \
+  ret name(__VA_ARGS__) suffix;                                                                                                                      \
+  static CONS(name, _t) & CONS($, name)() {                                                                                                          \
+    static CONS(name, _t) fn;                                                                                                                        \
+    return fn;                                                                                                                                       \
+  }                                                                                                                                                  \
+  ret CONS($$, name)(__VA_ARGS__) suffix
+#define IMPL_STATIC(ret, name, ...)                                                                                                                  \
+  using CONS(name, _t) = ret (*)(__VA_ARGS__);                                                                                                       \
+  static ret name(__VA_ARGS__);                                                                                                                      \
+  static CONS(name, _t) & CONS($, name)() {                                                                                                          \
+    static CONS(name, _t) fn;                                                                                                                        \
+    return fn;                                                                                                                                       \
+  }                                                                                                                                                  \
+  static ret CONS($$, name)(__VA_ARGS__)
+}
---
+++ mods.h
@@ -0,0 +1,2 @@
+#pragma once
+#define EXPORT extern "C" __attribute__((__visibility__("default")))
